c4a80aa04cf84c9885410cd7f57410d72b8100cb
ansel <populator@live.com>
2025-01-19 21:19:24 +0500

    dev

--- deploy.py
+++ deploy.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
 import time
+import shutil
 
+# Конфигурация
 PROJECT_NAME = "neft"
 GITHUB_REPO = "https://github.com/lizzardarcher/neft.git"
 DOMAIN = "rusgeolog.ru"
@@ -11,36 +13,47 @@ DB_USER = "django_user"
 DB_PASSWORD = "k*hvcnUBcNN^g740kkkj8J&HVvcbk"
 DOCKER_IMAGE_NAME = f"{PROJECT_NAME}-image"
 DOCKER_CONTAINER_NAME = f"{PROJECT_NAME}-container"
-
+WORK_DIR = "/opt"
+USER = 'root'
 
 def run_command(command, check=True):
+    """Запускает команду в shell."""
+    print(f"Running: {command}")
     try:
         subprocess.run(command, shell=True, check=check, executable='/bin/bash')
     except subprocess.CalledProcessError as e:
+        print(f"Command failed: {e}")
         exit(1)
 
 
 def update_system():
+    """Обновляет систему и устанавливает зависимости."""
+    print("Updating system and installing dependencies...")
     os.system("sudo apt update -y")
     os.system("sudo apt upgrade -y")
-    os.system("sudo apt install -y python3 python3-pip git docker.io docker-compose nginx mysql-server")
+    os.system("sudo apt install -y python3 python3-pip git nginx mysql-server")
     os.system("sudo apt install -y python3.12-venv")
-    os.system("sudo systemctl enable docker")
-    os.system("sudo systemctl start docker")
     os.system("sudo systemctl enable nginx")
 
 
 def clone_repo():
+    """Клонирует репозиторий с GitHub."""
+    print(f"Cloning repository from {GITHUB_REPO}...")
     os.system(f"git clone {GITHUB_REPO} {PROJECT_NAME}")
     os.chdir(PROJECT_NAME)
 
 
 def create_virtualenv():
+    """Создаёт виртуальное окружение и устанавливает зависимости."""
+    print("Creating virtual environment and installing dependencies...")
+    os.chdir(f"{WORK_DIR}/{PROJECT_NAME}")
     os.system("python3 -m venv venv")
     os.system(f"source venv/bin/activate && pip install -r requirements.txt")
 
 
 def create_mysql_database():
+    """Создает базу данных MySQL."""
+    print("Creating MySQL database...")
     os.system(f"sudo mysql -e 'CREATE DATABASE IF NOT EXISTS {DB_NAME};'")
     os.system(f"sudo mysql -e \"CREATE USER IF NOT EXISTS '{DB_USER}'@'localhost' IDENTIFIED BY '{DB_PASSWORD}';\"")
     os.system(f"sudo mysql -e \"GRANT ALL PRIVILEGES ON {DB_NAME}.* TO '{DB_USER}'@'localhost';\"")
@@ -48,16 +61,76 @@ def create_mysql_database():
 
 
 def create_ssl_certificate():
+    """Создает бесплатный SSL-сертификат с Let's Encrypt."""
+    print("Creating SSL certificate with Let's Encrypt...")
     os.system("sudo apt install -y certbot python3-certbot-nginx")
     os.system(f"sudo certbot --nginx -d {DOMAIN} -m {EMAIL} --non-interactive --agree-tos")
 
 
+def create_gunicorn_service():
+    """Создает сервис systemd для Gunicorn."""
+    gunicorn_cmd = f"/opt/{PROJECT_NAME}/venv/bin/gunicorn -c gunicorn-cfg.py {PROJECT_NAME}.wsgi:application"  # Предполагает наличие wsgi файла
+    service_file = f"""
+[Unit]
+Description=Gunicorn service for {PROJECT_NAME}
+Requires={PROJECT_NAME}-gunicorn.socket
+After=network.target
+
+[Service]
+Type=notify
+NotifyAccess=main
+User={USER}
+RuntimeDirectory=gunicorn
+WorkingDirectory=/opt/{PROJECT_NAME}
+Environment="PYTHONPATH=/opt/{PROJECT_NAME}"
+ExecStart={gunicorn_cmd}
+ExecReload=/bin/kill -s HUP $MAINPID
+KillMode=mixed
+TimeoutStopSec=5
+PrivateTmp=true
+Restart=always
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+"""
+    with open(f"/etc/systemd/system/{PROJECT_NAME}-gunicorn.service", "w") as f:
+        f.write(service_file)
+    os.system("systemctl daemon-reload")
+    os.system(f"systemctl enable {PROJECT_NAME}-gunicorn.service")
+    os.system(f"systemctl start {PROJECT_NAME}-gunicorn.service")
+
+
+def create_gunicorn_socket():
+    """Создает сокет systemd для Gunicorn."""
+    socket_file = f"""
+[Unit]
+Description=gunicorn socket for {PROJECT_NAME}
+
+[Socket]
+ListenStream=/run/gunicorn.sock
+SocketUser=www-data
+SocketGroup=www-data
+SocketMode=0660
+
+[Install]
+WantedBy=sockets.target
+"""
+    with open(f"/etc/systemd/system/{PROJECT_NAME}-gunicorn.socket", "w") as f:
+        f.write(socket_file)
+    os.system("systemctl daemon-reload")
+    os.system(f"systemctl enable {PROJECT_NAME}-gunicorn.socket")
+    os.system(f"systemctl start {PROJECT_NAME}-gunicorn.socket")
+
+
 def create_nginx_config():
+    """Создает конфигурацию Nginx."""
+    print("Creating Nginx configuration...")
     nginx_config = f"""
 server {{
     listen 80;
     server_name {DOMAIN};
-    return 301 https://$host$request_uri;
+    # return 301 https://$host$request_uri;
 }}
 
 server {{
@@ -67,14 +140,16 @@ server {{
     ssl_certificate /etc/letsencrypt/live/{DOMAIN}/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/{DOMAIN}/privkey.pem;
 
+    ssl_protocols       TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
     include /etc/letsencrypt/options-ssl-nginx.conf;
-
+    client_max_body_size 1G;
+    
     location / {{
-        proxy_pass http://unix:/tmp/{PROJECT_NAME}.socket;
+        proxy_pass http://unix:/run/gunicorn.sock;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        # proxy_set_header X-Forwarded-Proto $scheme;
     }}
 
     location /static/ {{
@@ -92,88 +167,43 @@ server {{
     os.system("sudo systemctl restart nginx")
 
 
-def create_docker_files():
-    dockerfile_content = f"""
-FROM python:3.12-slim
-WORKDIR /{PROJECT_NAME}
-COPY . /{PROJECT_NAME}
-RUN python3 -m pip install -r requirements.txt
-COPY . /{PROJECT_NAME}
-EXPOSE 8000
-CMD ["gunicorn", "--bind", "unix:/tmp/{PROJECT_NAME}.socket",  "--workers=5", "{PROJECT_NAME}.wsgi:application"]
-"""
-    with open(f"{PROJECT_NAME}/Dockerfile", "w") as f:
-        f.write(dockerfile_content)
-
-    docker_compose_content = f"""
-version: "3.9"
-services:
-  web:
-    build: .
-    container_name: {DOCKER_CONTAINER_NAME}
-    volumes:
-      - .:/{PROJECT_NAME}
-    restart: always
-    """
-    with open(f"{PROJECT_NAME}/docker-compose.yml", "w") as f:
-        f.write(docker_compose_content)
-
-
-def build_docker_image():
-    os.chdir(f"/opt/{PROJECT_NAME}")
-    os.system(f"docker build -t {DOCKER_IMAGE_NAME} .")
-
-
-def run_docker_container():
-    os.chdir(f"/opt/{PROJECT_NAME}")
-    os.system("docker-compose up -d")
-
-
-def create_systemd_service():
-    service_content = f"""
-[Unit]
-Description=Gunicorn daemon for {PROJECT_NAME}
-After=network.target
-
-[Service]
-User=ubuntu
-Group=ubuntu
-WorkingDirectory=/opt/{PROJECT_NAME}
-ExecStart=/usr/bin/docker-compose up
-ExecStop=/usr/bin/docker-compose down
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-"""
-    with open(f"/etc/systemd/system/{PROJECT_NAME}.service", "w") as f:
-        f.write(service_content)
-    os.system(f"sudo systemctl daemon-reload")
-    os.system(f"sudo systemctl enable {PROJECT_NAME}.service")
-    os.system(f"sudo systemctl start {PROJECT_NAME}.service")
-
-
 def collect_static():
+    """Собирает статические файлы."""
+    print("Collecting static files...")
+    os.chdir(f"{WORK_DIR}/{PROJECT_NAME}")
     os.system(f"source venv/bin/activate && python3 manage.py collectstatic --noinput")
 
 
 def migrate_db():
+    """Мигрирует базу данных."""
+    print("Migrating database...")
+    os.chdir(f"{WORK_DIR}/{PROJECT_NAME}")
     os.system(f"source venv/bin/activate && python3 manage.py migrate")
 
 
 def main():
     update_system()
+
     clone_repo()
+
     create_virtualenv()
+
     create_mysql_database()
+
     create_ssl_certificate()
+
+    create_gunicorn_service()
+
+    create_gunicorn_socket()
+
     create_nginx_config()
-    create_docker_files()
-    build_docker_image()
-    run_docker_container()
+
     collect_static()
+
     migrate_db()
-    create_systemd_service()
+
+    print("Deployment complete!")
+
 
 if __name__ == "__main__":
     main()
--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,7 +1,7 @@
 # # -*- encoding: utf-8 -*-
 
-command = '/opt/neft/env/bin/gunicorn'
-pythonpath = '/opt/dicom_converter'
+command = '/opt/neft/venv/bin/gunicorn'
+pythonpath = '/opt/neft'
 bind = '0.0.0.0'
 workers = 5
 timeout = 600
